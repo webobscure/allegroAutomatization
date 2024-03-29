431b8957562261f027539cee5a22a5fce0530992
Alexandr <50839940+webobscure@users.noreply.github.com>
2024-03-29 17:35:09 +0300

    start project

---
+++ allegroAutomatization/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import generateExcel from 'zipcelx'
+
+function App() {
+
+  const config = {
+    filename: 'allegro-doc',
+    sheet: {
+      data: []
+    }
+  }
+
+  return (
+    <>
+        <table>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th>Version: 5.1 (en), updated: 14.03.2024</th>
+            <th style={{color: "red"}}>First 3 rows are for Allegro usage, do not change them. Do not remove any columns.</th>
+          </tr>
+          <tr>
+            <td>hello</td>
+            <td>hello</td>
+            <td>hello</td>
+            <td>hello</td>
+          </tr><tr>
+            <td>hello</td>
+            <td>hello</td>
+            <td>hello</td>
+            <td>hello</td>
+          </tr><tr>
+            <td>hello</td>
+            <td>hello</td>
+            <td>hello</td>
+            <td>hello</td>
+          </tr>
+        </table>
+        <button onClick={() =>  generateExcel(config)}>
+          Generate Excel document 
+        </button>
+      
+    </>
+  )
+}
+
+export default App
