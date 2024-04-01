288c74a676f5b02624a7dfd366bd68a6030f1943
Alexandr <50839940+webobscure@users.noreply.github.com>
2024-04-01 17:59:15 +0300

    разметка таблицы

--- allegroAutomatization/src/App.jsx
+++ allegroAutomatization/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 import generateExcel from 'zipcelx'
 
 function App() {
+useEffect(() => {
+  fetch('https://50f2fa.myshopify.com/admin/api/2023-01/products.json?limit=120&fields=variants', {
+    headers: {
+      'X-Shopify-Access-Token': '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',
+      'rel': 'next'
+    }
+  }).then((arr) => {
+    arr.forEach((key) => {
+      let res = []
+      res = key 
+
+      console.log(res)
+    })
+  })
+
+  
+}, [])
+
 
   const config = {
     filename: 'allegro-doc',
@@ -16,6 +33,7 @@ function App() {
   return (
     <>
         <table>
+          <thead>
           <tr>
             <th></th>
             <th></th>
@@ -31,10 +49,33 @@ function App() {
             <th>Basic Information</th>
             <th>Tax parameters</th>
             <th>Terms of Sales</th>
-            <th>Order Information</th>
-            <th>Order Information</th>
-
+            <th>Additional specifications</th>
+            <th>Product features</th>
           </tr>
+          <tr>
+            <th>Product ID (EAN/UPC/ISBN/ISSN/Allegro Product ID)</th>
+            <th>Main Category</th>
+            <th>Leaf Category</th>
+            <th>Reference number/Seller SKU</th>
+            <th>Price</th>
+            <th>Title</th>
+            <th>Images</th>
+            <th>Offer Description</th>
+            <th>Invoice</th>
+            <th>Offer Subject</th>
+            <th>VAR rates</th>
+            <th>Returns Terms </th>
+            <th>Complaints Terms</th>
+            <th>Warranties (optional)</th>
+            <th>Person responsible for product compliance</th>
+            <th>Packaging Status</th>
+            <th>Brand</th>
+            <th>Model</th>
+            <th>Manufacturers code</th>
+            
+          </tr>
+          </thead>
+          <tbody>
           <tr>
             <td>hello</td>
             <td>hello</td>
@@ -51,6 +92,7 @@ function App() {
             <td>hello</td>
             <td>hello</td>
           </tr>
+          </tbody>
         </table>
         <button onClick={() =>  generateExcel(config)}>
           Generate Excel document 
